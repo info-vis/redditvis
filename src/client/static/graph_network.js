ef788565b5b315d9594a0f7acbb6eff2790b5f70
Misha Rigot <misharigot@gmail.com>
2021-02-26 15:46:24 +0100

    WIP drag, zoom and pan

--- src/client/static/graph_network.js
+++ src/client/static/graph_network.js
@@ -19,7 +19,7 @@ Vue.component('graph-network', {
             this.data = await data
         }
     },
-    created: async function() {
+    mounted: async function() {
         const width = this.width
         const height = this.height
 
@@ -34,6 +34,7 @@ Vue.component('graph-network', {
         const links = this.data.links.map(d => ({source: d[0], target: d[1], value: d[2]}))
         const nodes = this.data.nodes.map(d => ({id: d, group: Math.floor(Math.random() * Math.floor(10))}))
       
+        const r = 1.5 // Used for the zoom
         const canvas = document.getElementById("graph-network-canvas")
         const context = canvas.getContext("2d")
 
@@ -58,47 +59,57 @@ Vue.component('graph-network', {
         function ticked() {
             clearCanvas()
             setBackgroundColor()
-
-            context.beginPath();
-            links.forEach(drawLink);
-            context.strokeStyle = "#aaa";
-            context.stroke();
-            
-            context.strokeStyle = "#fff";
-            for (const node of nodes) {
-                context.beginPath();
-                context.lineWidth = 1;
+            drawLinks(links)            
+            for (node of nodes) {
                 drawNode(node) 
-                context.fillStyle = color(node);
-                context.fill();
-                context.stroke();
             }
         }
 
-        function drawLink(d) {
-            context.moveTo(d.source.x, d.source.y);
-            context.lineTo(d.target.x, d.target.y);
-            context.lineWidth = d.value/15;
+        function drawLinks(links) {
+            context.beginPath();
+            links.forEach(link => {
+                context.moveTo(link.source.x, link.source.y);
+                context.lineTo(link.target.x, link.target.y);
+                context.lineWidth = link.value/15;
+            })
+            context.strokeStyle = "#aaa";
+            context.stroke();
         }
     
-        function drawNode(d) {
-            const centerX = d.x
-            const centerY = d.y
-            const startAngle = 0
-            const endAngle = 2 * Math.PI
-            const foo = 5
-            context.moveTo(centerX + foo, centerY);
-            context.arc(centerX, centerY, foo, startAngle, endAngle);
+        function drawNode(node) {
+            context.strokeStyle = "#fff";
+            context.beginPath();
+            context.lineWidth = 1;
+            const radius = 5
+            context.moveTo(node.x + radius, node.y);
+            context.arc(node.x, node.y, radius, 0, 2 * Math.PI);
+
+            context.fillStyle = color(node);
+            context.fill();
+            context.stroke();
         }
 
         simulation.on("tick", ticked);
 
         drag = simulation => {
             function dragsubject(event) {
-                return simulation.find(event.x, event.y);
+                const transform = d3.zoomTransform(canvas)
+                let subject = null
+                let distance = 5
+                const x = transform.invertX(event.x)
+                const y = transform.invertY(event.y)
+                for (const node of nodes) {
+                    let d = Math.hypot(x - node.x, y - node.y)
+                    if (d < distance) {
+                        distance = d
+                        subject = node
+                    }
+                }
+                return subject ? simulation.find(event.x, event.y) : null
             }
           
             function dragstarted(event) {
+                console.log(event)
                 if (!event.active) simulation.alphaTarget(0.3).restart();
                 event.subject.fx = event.subject.x;
                 event.subject.fy = event.subject.y;
@@ -123,8 +134,31 @@ Vue.component('graph-network', {
                 .on("end", dragended);
         }
 
-        d3.select(context.canvas).call(drag(simulation)).node();
+        function zoomed(transform) {
+            context.save();
+            clearCanvas()
+            setBackgroundColor()
+            context.translate(transform.x, transform.y);
+            context.scale(transform.k, transform.k);
+            drawLinks(links)
+            for (const node of nodes) {
+                drawNode(node) 
+            }
+            // for (const [x, y] of data) {
+            //     context.moveTo(x + r, y);
+            //     context.arc(x, y, r, 0, 2 * Math.PI);
+            // }
+            context.fill();
+            context.restore();
+        }
 
+        d3.select(context.canvas).call(drag(simulation)).node();
+        d3.select(context.canvas).call(d3.zoom()
+            .scaleExtent([-1, 8])
+            .on("zoom", ({transform}) => zoomed(transform)))
+            .on("wheel", event => event.preventDefault());
+        
+        zoomed(d3.zoomIdentity);
     },
     template: `
         <div>
