1f7db684a897151d6d1187501756f66399b474d1
gtama7 <gyte.tama@gmail.com>
2021-03-07 14:46:03 +0100

    reactive title

--- src/api/routes.py
+++ src/api/routes.py
@@ -55,7 +55,7 @@ def top_properties():
 	else: 
 		plot_title = "Top properties of all subreddits"
 	
-	p=figure(y_range=list(reversed(data.index)), plot_height=300, plot_width=350, x_range=(0, 1), toolbar_location=None, tools="")
+	p=figure(y_range=list(reversed(data.index)), plot_height=300, plot_width=350, x_range=(0, 0.5), toolbar_location=None, tools="")
 	p.hbar(y=list(data.index), right=list(data.values), left=0, height=0.9)
 	p.asterisk(y=list(dataavg.index), x=list(dataavg.values), color="midnightblue", legend_label="Avg. of all subreddits",)
 	p.ygrid.grid_line_color = None
--- src/client/static/properties_plot.js
+++ src/client/static/properties_plot.js
@@ -1,7 +1,8 @@
 Vue.component('properties-plot', {
     data: function () {
         return {
-            isLoading: false
+            isLoading: false,
+            plotTitle: ""
         }
     },
     props: {
@@ -18,12 +19,16 @@ Vue.component('properties-plot', {
             let url = `${apiEndpoint}top-properties`
             let sourceSubredditQuery = `source-subreddit=${this.sourceSubreddit}`
             let targetSubredditQuery = `target-subreddit=${this.targetSubreddit}`
+            this.plotTitle = "Top properties of all subreddits"
             if (this.sourceSubreddit && this.targetSubreddit) {
                 url = url + "?" + sourceSubredditQuery + "&" + targetSubredditQuery 
+                this.plotTitle = `Top properties of the subreddit: ${this.sourceSubreddit} with the target subreddit: ${this.targetSubreddit}`
             } else if (this.sourceSubreddit) {
                 url = url + "?" + sourceSubredditQuery
+                this.plotTitle = `Top properties of the subreddit: ${this.sourceSubreddit}`
             } else if (this.targetSubreddit) {
                 url = url + "?" + targetSubredditQuery
+                his.plotTitle = `Top properties of the subreddit as a target: ${this.targetSubreddit}`
             }
             const propertiesResponse = await fetch(url);
             const propertiesPlot = await propertiesResponse.json();
@@ -44,6 +49,17 @@ Vue.component('properties-plot', {
             <div class="spinner-grow my-5" role="status">
             </div>
         </div>
+        <div v-show="!isLoading">
+            <p class="mb-0 mt-1" >
+            <small> {{plotTitle}} </strong></small>
+            </p>
+        </div>
         <div v-show="!isLoading" id="properties-plot" class="bk-root"></div>
     </div> `
 })
+
+/**
+ * 1. Display some arbitrary title that is not dynamic but just in the right position (only when the plot is loaded)
+ * 2. Display the title based on what is in plotTitle
+ * 3. Change the value of plotTitle based on the fetch
+ */
