1fc9f370e118b351c67e1db80e0efe5abd1012fe
AGoetzee <arthurgoetzee@outlook.com>
2021-03-17 19:42:03 +0100

    implemented sentimentbox grid

--- src/client/static/sentiment_box.js
+++ src/client/static/sentiment_box.js
@@ -2,8 +2,10 @@ Vue.component('sentiment-box', {
     data: function() {
         return {
             data: null,
-            width: 400,
-            barHeight: 50,
+            width: 700,
+            height: 700,
+            cellSize: 15,
+            years: ['2014','2015','2016','2017'],
             colors: ['#00ff00','#ff0900', '#00000045'],
             isLoading: false
         }
@@ -13,18 +15,19 @@ Vue.component('sentiment-box', {
             type: String,
             default: null
         },
+        targetSubreddit: {
+            type: String,
+            default: null
+        }
     },
     watch: {
-        sourceSubreddit: "createPlot"
+        sourceSubreddit: "createPlot",
+        targetSubreddit: "createPlot"
     },
     computed: {
-        barWidth(){
-            return this.width / this.data.length
-        },
-        strokeWidth(){
-            return (this.widht / this.data.lenght) * 0.05
+        yearHeight(){
+            return this.cellSize * 7 + 15
         }
-        // this.strokeWidth = this.barWidth*0.05
     },
 
     methods: {
@@ -55,30 +58,86 @@ Vue.component('sentiment-box', {
             if (!this.data) {
                 return
             }
-            console.log(this.data.length)
-            var graph = d3.select("#sentiment-box")
+            const tParser = d3.timeParse("%Y-%m-%d")
+            var dateRange = d3.timeDays(new Date(2014, 1, 1), new Date(2017, 12, 31));
+            
+            console.log(dateRange)
+           
+            
+
+            let arr = this.data.values
+            const maxValue = Math.max(arr);
+            const minValue = Math.min(arr);
+
+            years = function(d){
+                (d => d3.timeDay(new Date(d).getUTCFullYear()))
+            }
+
+            var svg = d3.select("#sentiment-box")
                       .append("svg")
                       .attr("width", this.width)
-                      .attr("height", this.barHeight);
+                      .attr("height", this.height);
     
-            var bar = graph.selectAll("g")
-                      .data(this.data)
-                      .enter()
-                      .append("g")
-                      .attr("transform", (d, i) => {
-                            return "translate(" + i * this.barWidth + ",0)";
-                      });
     
-            bar.append("rect")
-            .attr("width", this.barWidth)
-            .attr("height", this.barHeight)
-            .attr("stroke-width",this.strokeWidth)
-            .attr("stroke", this.colors[2])
-            .attr("fill", (d)=>{
-                if (d < 1) {
-                    return this.colors[1];
-                } else {return this.colors[0];}
-            });
+            const group = svg.append("g");
+
+            const year = group
+            .selectAll("g")
+            .data(this.years)
+            .join("g")
+            .attr(
+                "transform",
+                (d, i) => `translate(50, ${this.yearHeight * i + this.cellSize * 1.5})`
+            );
+            
+            year
+            .append("text")
+            .attr("x", -5)
+            .attr("y", -30)
+            .attr("text-anchor", "end")
+            .attr("font-size", 16)
+            .attr("font-weight", 550)
+            .attr("transform", "rotate(270)")
+            .text(d => d);
+  
+          const formatDay = d =>
+            ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"][d.getUTCDay()];
+          const countDay = d => d.getUTCDay();
+          const timeWeek = d3.utcSunday;
+          const formatDate = d3.utcFormat("%x");
+          const colorFn = d3
+            .scaleSequential(d3.interpolateBuGn)
+            .domain([Math.floor(minValue), Math.ceil(maxValue)]);
+          const format = d3.format("+.2%");
+  
+
+          year
+            .append("g")
+            .attr("text-anchor", "end")
+            .selectAll("text")
+            .data(d3.range(7).map(i => new Date(2014, 0, i)))
+            .join("text")
+            .attr("x", -5)
+            .attr("y", d => (countDay(d) + 0.5) * this.cellSize)
+            .attr("dy", "0.31em")
+            .attr("font-size", 12)
+            .text(formatDay);
+            
+
+        
+
+          year
+            .append("g")
+            .selectAll("rect")
+            .data(dateRange)
+            .join("rect")
+            .attr("width", this.cellSize - 1.5)
+            .attr("height", this.cellSize - 1.5)
+            .attr(
+              "x",
+              (d, i) => timeWeek.count(d3.utcYear(d), d) * this.cellSize + 10)
+            .attr("y", (d,i) => countDay(d) * this.cellSize + 0.5)
+            .attr("fill", 'Grey')
         },
         
     },
