9c0d92b0aa4e82ff92894e0d03ea28984c9198ae
AGoetzee <55135668+AGoetzee@users.noreply.github.com>
2021-03-27 20:19:41 +0100

    Changed json.dumps to jsonify in routes.py
    Co-authored-by: Misha Rigot <misharigot@gmail.com>

--- src/api/routes.py
+++ src/api/routes.py
@@ -27,7 +27,7 @@ def sentiment_box():
 	
 	sentiments = BodyModel.getInstance().get_sentiments(target_subreddit, source_subreddit)
   
-	return json.dumps(sentiments)
+	return jsonify((sentiments)
 
 @bp.route('/top-properties')
 def top_properties():
