ab38f3ea44cfe6d817b7431921d9b77c34fa04a2
Misha Rigot <misharigot@gmail.com>
2021-04-01 15:12:48 +0200

    Added dark theme (#45)
    * Added dark theme
    
    * Added transparency to highlight color in network

--- src/client/static/sidebar_container.js
+++ src/client/static/sidebar_container.js
@@ -2,7 +2,7 @@ Vue.component("sidebar-container", {
     template: `
     <div class="row">
         <div class="col">
-            <div class="border p-2 rounded border mb-2 shadow-sm" style="background-color: white">
+            <div class="border p-2 rounded border mb-2 shadow-sm">
                 <slot></slot>
             </div>
         </div>
